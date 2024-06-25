f28ee5982a55f7db892111414e7e4b760d98ace4
sergioperess <sergio_kaun@hotmail.com>
2024-06-25 09:15:57 -0300

    Transações

--- src/main/java/com/example/cadastro/service/TransactionService.java
+++ src/main/java/com/example/cadastro/service/TransactionService.java
@@ -2,6 +2,10 @@ package com.example.cadastro.service;
 
 import com.example.cadastro.entity.Transaction;
 
+import java.util.List;
+
 public interface TransactionService {
     public Transaction save(Transaction transaction);
+
+    public List<Transaction> findAll();
 }
--- src/main/java/com/example/cadastro/service/imple/ITransactionService.java
+++ src/main/java/com/example/cadastro/service/imple/ITransactionService.java
@@ -1,9 +1,13 @@
 package com.example.cadastro.service.imple;
 
 import com.example.cadastro.entity.Transaction;
+import com.example.cadastro.entity.User;
 import com.example.cadastro.repository.TransactionRepository;
 import com.example.cadastro.service.TransactionService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ITransactionService implements TransactionService {
@@ -15,11 +19,17 @@ public class ITransactionService implements TransactionService {
 
     /**
      * Método utilizado para cadastrar uma nova transação
+     *
      * @param transaction Transação
      * @return Transação
      */
     @Override
     public Transaction save(Transaction transaction) {
         return this.transactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> findAll() {
+        return this.transactionRepository.findAll();
     }
 }
