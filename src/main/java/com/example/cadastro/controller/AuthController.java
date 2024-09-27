111f006288123c96b633d4b6c35ba569b1ae22c7
sergioperess <sergio_kaun@hotmail.com>
2024-09-27 18:59:20 -0300

    Correção de bugs

--- src/main/java/com/example/cadastro/controller/AuthController.java
+++ src/main/java/com/example/cadastro/controller/AuthController.java
@@ -9,9 +9,6 @@ import com.example.cadastro.repository.UserRepository;
 import com.example.cadastro.security.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,7 +36,7 @@ public class AuthController {
     @PostMapping("/login")
     @Operation(summary = "Login do usuário",
             description = "Método utilizado para fazer o login de um Usuário e gerar um token para autenticação")
-    public ResponseEntity login(HttpServletResponse response, @RequestBody LoginDTO body){
+    public ResponseEntity login(@RequestBody LoginDTO body){
 
         // Utilizado para procurar o usuário no banco de dados
         User user = this.repository.findByEmail(body.email())
@@ -48,16 +45,6 @@ public class AuthController {
         if(passwordEncoder.matches(body.senha(), user.getSenha())){
             // Se as senhas forem iguais, gera um token para o usuário
             String token = this.tokenService.generateToken(user);
-
-            // Criando o cookie com o token JWT
-            Cookie authCookie = new Cookie("authToken", token);
-            authCookie.setMaxAge(7 * 24 * 60 * 60); // Definindo o tempo de expiração para 7 dias
-            authCookie.setHttpOnly(true); // Tornando o cookie acessível apenas via HTTP (não acessível por JavaScript)
-            authCookie.setSecure(false); // Definindo o cookie para não ser transmitido apenas via HTTPS (recomendado em produção)
-            authCookie.setPath("/"); // Tornando o cookie disponível em toda a aplicação
-
-            // Adicionando o cookie à resposta HTTP
-            response.addCookie(authCookie);
 
             return ResponseEntity.ok(new LoginView(user.getId(),user.getEmail(), token));
         }
@@ -87,50 +74,9 @@ public class AuthController {
         return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/setCookie")
-    public String setCookie(String token, HttpServletResponse response) {
-        // Criando o cookie com o token JWT
-        Cookie authCookie = new Cookie("authToken", token);
-        authCookie.setMaxAge(7 * 24 * 60 * 60); // Definindo o tempo de expiração para 7 dias
-        authCookie.setHttpOnly(true); // Tornando o cookie acessível apenas via HTTP (não acessível por JavaScript)
-        authCookie.setSecure(false); // Definindo o cookie para não ser transmitido apenas via HTTPS (recomendado em produção)
-        authCookie.setPath("/"); // Tornando o cookie disponível em toda a aplicação
 
-        // Adicionando o cookie à resposta HTTP
-        response.addCookie(authCookie);
 
-        return "Login bem-sucedido! O token foi armazenado no cookie.";
-    }
 
-    @GetMapping("/get-token")
-    public String getToken(HttpServletRequest request) {
-        // Obtém todos os cookies da requisição
-        Cookie[] cookies = request.getCookies();
-
-        // Verifica se há cookies e procura pelo cookie de autenticação
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("authToken")) {
-                    // Retorna o valor do token armazenado no cookie
-                    return "Token encontrado: " + cookie.getValue();
-                }
-            }
-        }
-        return "Token não encontrado!";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        // Criando um cookie com o mesmo nome e valor nulo, para remover
-        Cookie authCookie = new Cookie("authToken", null);
-        authCookie.setMaxAge(0); // Define o tempo de vida como 0 para remover o cookie
-        authCookie.setPath("/"); // Certifique-se de que o caminho seja o mesmo do cookie original
-
-        // Adicionando o cookie à resposta para removê-lo do navegador
-        response.addCookie(authCookie);
-
-        return "Logout realizado! O token foi removido.";
-    }
 
 }
 
